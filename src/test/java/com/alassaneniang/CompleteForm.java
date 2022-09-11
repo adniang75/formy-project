d99eac3573d2dea32562f98f5b5efc0c2d0fd122
Alassane NIANG <adniang75@gmail.com>
2022-09-12 01:37:51 +0200

    Running test using Selenium Grid

--- src/test/java/com/alassaneniang/CompleteForm.java
+++ src/test/java/com/alassaneniang/CompleteForm.java
@@ -3,15 +3,20 @@ package com.alassaneniang;
 import com.alassaneniang.pages.ConfirmationPage;
 import com.alassaneniang.pages.FormPage;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CompleteForm {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException {
 
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        WebDriver driver = new RemoteWebDriver(new URL(FormyProject.REMOTE_HUB_URL), options);
         driver.get(FormyProject.BASE_URL + "/form");
 
         FormPage formPage = new FormPage();
--- src/test/java/com/alassaneniang/FormyProject.java
+++ src/test/java/com/alassaneniang/FormyProject.java
@@ -3,5 +3,6 @@ package com.alassaneniang;
 public class FormyProject {
 
     public final static String BASE_URL = "https://formy-project.herokuapp.com";
+    public final static String REMOTE_HUB_URL = "http://192.168.1.20:4444";
 
 }
