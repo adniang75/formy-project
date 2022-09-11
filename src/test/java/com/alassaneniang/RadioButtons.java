ef10460dae467ec7403a4ae1e0d3135190d15f8a
Alassane NIANG <adniang75@gmail.com>
2022-09-11 21:27:12 +0200

    Radio Buttons

---
+++ src/test/java/com/alassaneniang/RadioButtons.java
@@ -0,0 +1,30 @@
+package com.alassaneniang;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class RadioButtons {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get(FormyProject.BASE_URL + "/radiobutton");
+
+        WebElement radioButton1 = driver.findElement(By.id("radio-button-1"));
+        radioButton1.click();
+        Thread.sleep(3000);
+
+        WebElement radioButton2 = driver.findElement(By.cssSelector("input[value='option2']"));
+        radioButton2.click();
+        Thread.sleep(3000);
+
+        WebElement radioButton3 = driver.findElement(By.xpath("/html/body/div/div[3]/input"));
+        radioButton3.click();
+        Thread.sleep(3000);
+
+        driver.quit();
+    }
+
+}
